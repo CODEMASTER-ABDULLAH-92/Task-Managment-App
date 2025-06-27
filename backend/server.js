36a1750ab1b2a0cd9f20d402d5957afb90dacaa6
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-06-27 12:06:54 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173",credentials:true}));
+app.use(cors({origin: "https://task-managment-app-frontend.vercel.app/",credentials:true}));
 app.use(morgan("dev"));
 
 
